9791592f0e835512ad5c3f36497b258aacaed81c
Adishor91 <adi_secheli@yahoo.com>
2024-08-10 16:00:29 +0300

    Created the React components: Header, Footer, Main Nav. Added the meta tags.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Header from './Header';
+import Nav from './Nav';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+      <div className="app-container">
+        <div className="header-nav-container">
+        <Header />
+        <Nav />
+        </div>
+      </div>
+      <Main />
+      <Footer />
+    </>
   );
 }
 
