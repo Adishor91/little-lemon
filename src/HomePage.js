577986136e2f54c4af95d9f95d76dec766272ae1
Adishor91 <adi_secheli@yahoo.com>
2024-08-12 23:28:03 +0300

    Began structuring the HomePage2

--- src/HomePage.js
+++ src/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
     return (
         <section className="homepage">
             <h1>Little Lemon</h1>
-            <h2>Chicago</h2>
+            <h3>Chicago</h3>
             <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
             <button class="reserve-button">Reserve a Table</button>
             <img src="/icons_assets/restauranfood.jpg" alt='Homepage'></img>
