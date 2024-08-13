3f3982774e22f6dc7c6efc5a700f9069a520e947
Adishor91 <adi_secheli@yahoo.com>
2024-08-13 11:46:24 +0300

    Continued styling the hero section

--- src/HomePage.js
+++ src/HomePage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const HomePage = () => {
     return (
-        <section className="homepage">
-            <h1>Little Lemon</h1>
-            <h3>Chicago</h3>
-            <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
-            <button class="reserve-button">Reserve a Table</button>
-            <img src="/icons_assets/restauranfood.jpg" alt='Homepage'></img>
+        <section className="homepage flex">
+            <div className="hero-text">
+                <h1>Little Lemon</h1>
+                <h3>Chicago</h3>
+                <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+                <button className="reserve-button">Reserve a Table</button>
+            </div>
+            <img src="/icons_assets/restauranfood.jpg" alt="Homepage" className="hero-image" />
         </section>
     );
 };
