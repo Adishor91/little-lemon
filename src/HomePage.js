c869ec902715c79f8079e1186dfb21119e2bc5f7
Adishor91 <adi_secheli@yahoo.com>
2024-08-14 12:15:11 +0300

    Did title and button for Specials

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,9 @@
 import React from "react";
+import Specials from "./Specials";
 
 const HomePage = () => {
     return (
-        <section className="homepage flex">
+        <><section className="homepage flex">
             <div className="hero-text">
                 <h1>Little Lemon</h1>
                 <h3>Chicago</h3>
@@ -11,6 +12,7 @@ const HomePage = () => {
             </div>
             <img src="/icons_assets/restauranfood.jpg" alt="Homepage" className="hero-image" />
         </section>
+        <Specials /></>
     );
 };
 
---
+++ src/Specials.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Specials = () => {
+    return (
+        <section className="specials">
+            <div className="title-and-button">
+                <h1>This weeks specials!</h1>
+                <button className="menu-button">Online menu</button>
+            </div>
+        </section>
+    );
+};
+
+export default Specials;
