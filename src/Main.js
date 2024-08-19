862c9f8cb619fa1089e55e28642b8513152d3288
Adishor91 <adi_secheli@yahoo.com>
2024-08-19 21:26:51 +0300

    finished linking. Next is coding the BookingPage component.

--- src/Main.js
+++ src/Main.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
-import BookingPage from './OurMenu';
+import BookingPage from './BookingPage';
 import OurMenu from './OurMenu';
 
 const Main = () => {
     return (
         <main className="main">
             <Routes>
-                 <Route path="/" element={<HomePage />}/>
-                 <Route path="/our-menu" element={<OurMenu />}/>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="/our-menu" element={<OurMenu />}/>
                 <Route path="/booking" element={<BookingPage />}/>
             </Routes>
         </main>
